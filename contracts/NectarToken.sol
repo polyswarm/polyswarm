e7d89e750934e5fe166e89552940fa98c2c3bf22
Maxwell Koo <mjkoo90@gmail.com>
2018-01-19 16:08:06 -0800

    Revert to old token for now, watch ERC827 going forward

--- contracts/NectarToken.sol
+++ contracts/NectarToken.sol
@@ -1,12 +1,22 @@
 pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/MintableToken.sol";
-//import "zeppelin-solidity/contracts/token/ERC827/ERC827Token.sol";
 
 
-//contract NectarToken is MintableToken, ERC827Token {
 contract NectarToken is MintableToken {
     string public name = "Nectar";
     string public symbol = "NCT";
     uint8 public decimals = 18;
+
+    /* Approves and then calls the receiving contract */
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+
+        //call the receiveApproval function on the contract you want to be notified. This crafts the function signature manually so one doesn't have to include a contract in here just for this.
+        //receiveApproval(address _from, uint256 _value, address _tokenContract, bytes _extraData)
+        //it is assumed that when does this that the call *should* succeed, otherwise one would use vanilla approve instead.
+        require(_spender.call(bytes4(bytes32(keccak256("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData));
+        return true;
+    }
 }
